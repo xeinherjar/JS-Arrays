1753f38288e303a9a08268edcb7f44474a2210ad
Alex Pate <proto.merkabah@gmail.com>
2015-01-15 15:39:27 -0500

    reverse()

--- ex.js
+++ ex.js
@@ -48,3 +48,11 @@ function multiply(arr) {
   });
   return mul;
 };
+
+function reverse(str) {
+  var rev_str = "";
+  for (var i = str.length -1; i >= 0; i--) {
+    rev_str += str[i];
+  }
+  return rev_str;
+}
